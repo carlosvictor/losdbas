d47fbd97fea09e6944f0452f289277121d7956c2
jademirf <jademirf@gmail.com>
2017-06-04 17:27:35 -0300

    Add files via upload

--- bd_olx_imoveis.sql
+++ bd_olx_imoveis.sql
@@ -34,9 +34,7 @@ MEMBER PROCEDURE exibe_funcionario (SELF tp_funcionario)
 ALTER TYPE tp_funcionario ADD ATTRIBUTE(supervisor REF tp_funcionario) CASCADE;
 
 #tabela funcionario
-CREATE TABLE tb_funcionario OF tp_funcionario(
-	cod PRIMARY KEY
-);
+CREATE TABLE tb_funcionario OF tp_funcionario;
 
 #inserindo valor à tabela funcionario
 INSERT INTO tb_funcionario VALUES(
@@ -47,6 +45,41 @@ INSERT INTO tb_funcionario VALUES(
 	TO_DATE('03-03-2005','DD-MM-YYYY'),
 	tp_fone(81,99999999),
 	tp_endereco(54400000,'tal de alguma coisa',20,'bl.02 ap302','Esse bairro','raincife','pe')
+);
+#tipo lista fones
+CREATE TYPE tp_lista_fones AS VARRAY(5) OF tp_fone;
+
+#tipo cliente
+CREATE TYPE tp_cliente AS OBJECT(
+	cod integer,
+	nome VARCHAR(30),
+	email varchar(30),
+	fone tp_lista_fones,
+	endereco tp_endereco
+);
+#pessoa fisica
+CREATE TYPE tp_pfisica UNDER tp_cliente(
+	cpf BIGINT,
+	rg varchar(30),
+	ocupacao varchar(30),
+	filiacao varchar(100),
+	data_nasc DATE
+);
+# pessoa juridica
+CREATE TYPE tp_pjuridica UNDER tp_cliente(
+	cnpj integer,
+	responsavel VARCHAR(40),
+	cpf_responsavel BIGINT
+);
+
+# tipo imovel
+CREATE TYPE tp_imovel AS OBJECT(
+	cod INTEGER,
+	descricao VARCHAR(255),
+	metros NUMBER(10,2),
+	imagens imagens,
+	endereco tp_endereco,
+	dono REF tp_cliente
 );
 
 
