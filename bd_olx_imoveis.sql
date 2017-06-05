f2fbcff95ac439b179a25c1fe7d6442a8f44997e
jademirf <jademirf@gmail.com>
2017-06-05 13:50:16 -0300

    Add files via upload
    com os //

--- bd_olx_imoveis.sql
+++ bd_olx_imoveis.sql
@@ -1,11 +1,11 @@
-#tipo telefone
+//tipo telefone
 CREATE TYPE tp_fone AS OBJECT(
 cod INTEGER,
 numero INTEGER,
 MEMBER PROCEDURE exibe_fone (SELF tp_fone)
 );
 /
-#tipo endereço
+//tipo endereço
 CREATE TYPE tp_endereco AS OBJECT(
 cep INTEGER,
 rua VARCHAR(20),
@@ -18,7 +18,7 @@ MEMBER PROCEDURE exibe_endereco (SELF tp_endereco)
 );
 /
 
-#tipo funcionario
+//tipo funcionario
 CREATE TYPE tp_funcionario AS OBJECT(
 cod varchar(10),
 nome VARCHAR(30),
@@ -30,13 +30,13 @@ endereco tp_endereco,
 MEMBER PROCEDURE exibe_funcionario (SELF tp_funcionario)
 );
 /
-#adicionando supervisor ao tipo funcionario
+//adicionando supervisor ao tipo funcionario
 ALTER TYPE tp_funcionario ADD ATTRIBUTE(supervisor REF tp_funcionario) CASCADE;
 
-#tabela funcionario
+//tabela funcionario
 CREATE TABLE tb_funcionario OF tp_funcionario;
 
-#inserindo valor à tabela funcionario
+//inserindo valor à tabela funcionario
 INSERT INTO tb_funcionario VALUES(
 	'func123',
 	'fulano', 
@@ -46,10 +46,10 @@ INSERT INTO tb_funcionario VALUES(
 	tp_fone(81,99999999),
 	tp_endereco(54400000,'tal de alguma coisa',20,'bl.02 ap302','Esse bairro','raincife','pe')
 );
-#tipo lista fones
+//tipo lista fones
 CREATE TYPE tp_lista_fones AS VARRAY(5) OF tp_fone;
 
-#tipo cliente
+//tipo cliente
 CREATE TYPE tp_cliente AS OBJECT(
 	cod integer,
 	nome varchar(30),
@@ -58,14 +58,14 @@ CREATE TYPE tp_cliente AS OBJECT(
 	endereco tp_endereco
 ) NOT FINAL INSTANTIABLE;
 
-#tabela cliente
+//tabela cliente
 CREATE TABLE tb_cliente OF tp_cliente;
 
-#view cliente
+//view cliente
 CREATE VIEW v_clientes OF tp_clientes WITH OBJECT ID(cod) AS
 SELECT cod,nome,email,fone,endereco FROM tb_cliente;
 
-#pessoa fisica
+//pessoa fisica
 CREATE TYPE tp_pfisica UNDER tp_cliente(
 	cpf integer,
 	rg varchar(30),
@@ -73,25 +73,25 @@ CREATE TYPE tp_pfisica UNDER tp_cliente(
 	filiacao varchar(100),
 	data_nasc DATE
 );
-# pessoa juridica
+// pessoa juridica
 CREATE TYPE tp_pjuridica UNDER tp_cliente(
 	cnpj integer,
 	responsavel VARCHAR(40),
 	cpf_responsavel integer
 );
-#view do tipo cliente
+//view do tipo cliente
 CREATE OR REPLACE VIEW SYSTEM.V_CLIENTE
 (SYS_NC_OID$,SYS_NC_ROWINFO$,COD,NOME,EMAIL,FONE,ENDERECO)
 AS
 SELECT cod, nome, email, fone,endereco FROM tb_cliente
 
-#tabela cliente pessoa Juridica
+//tabela cliente pessoa Juridica
 CREATE TABLE tb_clientepj OF tp_pjuridica;
 
-#tabela cliente pessoa fisica
+//tabela cliente pessoa fisica
 CREATE TABLE tb_clientepf OF tp_pfisica;
 
-# tipo imovel
+// tipo imovel
 CREATE TYPE tp_imovel AS OBJECT(
 	cod INTEGER,
 	descricao VARCHAR(255),
@@ -101,28 +101,28 @@ CREATE TYPE tp_imovel AS OBJECT(
 	dono REF tp_cliente
 ) NOT FINAL INSTAntIABLE;
 
-#tipo casa
+//tipo casa
 CREATE TYPE tp_casa UNDER tp_imovel(
 	area_construida NUMBER(10,2),
 	qtd_andares INTEGER,
 	banheiros INTEGER,
 	quartos INTEGER
 ) NOT FINAL;
-#tipo serviços e varray de serviços que serão adicionados às salas comerciais
+//tipo serviços e varray de serviços que serão adicionados às salas comerciais
 CREATE TYPE tp_servico AS OBJECT(
 	nome VARCHAR(30),
 	descricao VARCHAR(255)
 );
 CREATE TYPE servicos AS VARRAY(10) OF tp_servico;
 
-#tipo salas comerciais
+//tipo salas comerciais
 CREATE TYPE tp_sala_comercial UNDER tp_imovel(
 	banheiros INTEGER,
 	condominio NUMBER(5,2),
 	lista_servicos servicos
 ) NOT FINAL;
 
-#tipo apartamento
+//tipo apartamento
 CREATE TYPE tp_apartamento UNDER tp_imovel(
 	suites INTEGER,
 	andar INTEGER,
@@ -131,29 +131,29 @@ CREATE TYPE tp_apartamento UNDER tp_imovel(
 	ventilacao VARCHAR(10)
 ) NOT FINAL;
 
-#tipo terreno
+//tipo terreno
 CREATE TYPE tp_terreno UNDER tp_imovel NOT FINAL;
 
-#tipo area rural
+//tipo area rural
 CREATE TYPE tp_rural UNDER tp_imovel (
 	area_construida NUMBER(10,2)
 ) NOT FINAL;
 
-#tabela subtipos de imovel
+//tabela subtipos de imovel
 CREATE TABLE tb_casa OF tp_casa;
 CREATE TABLE tb_apartamento OF tp_apartamento;
 
-#tipo servico oferecido pela imobiliaria
+//tipo servico oferecido pela imobiliaria
 CREATE TYPE tp_servico_contrato AS OBJECT(
 	cod INTEGER,
 	tipo varchar(20),
 	descricao varchar(255)
 );
 
-#tabela de servicos da imobiliaria
+//tabela de servicos da imobiliaria
 CREATE TABLE tb_servico_contrato OF tp_servico_contrato;
 
-#tipo visita
+//tipo visita
 CREATE TYPE tp_visitas AS OBJECT(
 	data_visita DATE,
 	id_imovel REF tp_imovel,
@@ -161,10 +161,10 @@ CREATE TYPE tp_visitas AS OBJECT(
 	id_funcionario REF tp_funcionario
 );
 
-#tabela visita
+//tabela visita
 CREATE TABLE tb_visita OF tp_visitas;
 
-#tipo contrato
+//tipo contrato
 CREATE TYPE tp_contrato AS OBJECT(
 	numero INTEGER,
 	valor NUMBER(10,2),
@@ -176,7 +176,7 @@ CREATE TYPE tp_contrato AS OBJECT(
 	id_funcionario REF tp_funcionario
 );
 
-#tabela contrato
+//tabela contrato
 CREATE TABLE tb_contrato OF tp_contrato;
 
 
